cf24ba29432862477cf123258d3d1b5ab6144523
Apxat <apxata@gmail.com>
2019-04-25 22:20:02 +0300

    сделал обновление главной странице при добавлении траты

--- index.php
+++ index.php
@@ -3,9 +3,6 @@ require_once("admin/connection.php");
 require_once("admin/db_function.php");
 require_once("function/expenses.php");
 
-require_once("html/header.php");
-require_once("html/main.php");
-
 if (isset($_POST['add_expense'])) {
   
 // if (isset($_POST['summ'])){
@@ -15,12 +12,14 @@ if (isset($_POST['add_expense'])) {
   
     $result = add_expense($balance_id, $expense_sum, $comment );
       if($result){
-      echo "Трата добавлена успешно";
+        header("Location: index.php");
       }else{
       echo "Что-то пошло не так";
       }
 } 
 
+require_once("html/header.php");
+require_once("html/main.php");
 
 // }
 // закрываем подключение
